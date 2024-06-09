1df9eff07f6c9c5e6b4a3ee3b4fdfd97d32895ab
DhruvG2003 <133965391+DhruvG2003@users.noreply.github.com>
2024-06-09 14:04:14 +0530

    Create pyramid.py

---
+++ week1/pyramid.py
@@ -0,0 +1,20 @@
+def lower_triangular(n):
+    for i in range(1, n + 1):
+        print("* " * i)
+
+def upper_triangular(n):
+    for i in range(n):
+        print("  " * i + "* " * (n - i))
+
+def pyramid(n):
+    for i in range(1, n + 1):
+        print(" " * (n - i) + "* " * i)
+
+# Example usage:
+n = 5
+print("Lower Triangular Pattern:")
+lower_triangular(n)
+print("\nUpper Triangular Pattern:")
+upper_triangular(n)
+print("\nPyramid Pattern:")
+pyramid(n)
